138151a93799c0d1354f85f712e05ed161acac7c
Debashish Chowdhury <rahul062002@gmail.com>
2025-06-22 21:15:54 +0600

    Updated lesson1.js with new features

--- lesson1.js
+++ lesson1.js
@@ -1,2 +1,90 @@
-let greet = "Hi hello";
+//Number and Bigint
+let num = 7;
+let bigint = 999999999n;
+console.log(num);
+console.log(bigint);
+
+//String
+let greet = "Hi";
 console.log(greet);
+
+//Boolean
+let istrue = true;
+let isfalse = false
+console.log(istrue);
+console.log(isfalse);
+
+//Null and Undefined
+let finishcoursetime = undefined;
+let grade = null;
+console.log(finishcoursetime);
+console.log(grade);
+function help() {
+
+}
+console.log(help);
+function help() {
+  return null;
+}
+console.log(help);
+
+//Object 
+const user = {}
+user.fname = "John";
+user.lname = "Cena";
+user.age = 45;
+user.move = "AA";
+console.log(user);
+
+const person = {
+  name: "Randy",
+  age: 40,
+  move: "RKO",
+}
+console.log(person);
+
+//Assignment Operators
+let number = 100;
+number*= 10;
+console.log(number);
+
+//String Interpolation/Concatenation
+let age = 40;
+
+let string = 'Ronaldo is ' + age + ' years old.';
+console.log(string);
+
+//Variables
+const currency = '$';
+let userIncome = 85000;
+console.log(currency + userIncome + ' is more than the average income.');
+
+//Undefined
+//variable can be defined using the term var, const, and let
+let a;
+
+console.log(a);
+
+//let Keyword
+let count;
+console.log(count);
+count = 10;
+console.log(count);
+
+//Methods
+
+//String .length
+let text = 'Hello, how are you?'
+console.log(text.length);
+
+//Math.random()
+console.log(Math.random());
+
+//Math.floor()
+console.log(Math.floor(5.95));
+
+/* Mutiline
+comments
+*/
+
+//Remainder / Modulo Operator
